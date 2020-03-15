b1d35d44c2b9767e9cd5042944336dcf4d1a2940
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2020-03-14 21:03:12 -0400

    Create an ABC to build handlers off of

---
+++ handlers/abc.py
@@ -0,0 +1,24 @@
+import abc
+import tempfile
+from os.path import abspath, expanduser, join
+
+
+class BaseHandler(metaclass=abc.ABCMeta):
+    """Base Handler class that defines abstract methods"""
+
+    @property
+    def tempdir(self):
+        return self.__dict__.setdefault("dir", tempfile.TemporaryDirectory()).name
+
+    def file(self, extension, directory=None):
+        return open(
+            abspath(join(self.tempdir, "script." + extension)) if directory is None else abspath(expanduser(directory)),
+            "w", encoding="utf-8")
+
+    @abc.abstractmethod
+    def run(self, code_file):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def build(self, code, directory=None):
+        raise NotImplementedError
