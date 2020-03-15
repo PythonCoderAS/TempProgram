3f362b8a98f5f308b93d929f1a29021945727f36
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2020-03-14 21:03:52 -0400

    Add python script handlers

---
+++ handlers/python.py
@@ -0,0 +1,35 @@
+from subprocess import check_call, run
+from sys import exit
+
+from .abc import BaseHandler
+
+
+class PythonHandler(BaseHandler):
+    """Run Python 3 programs."""
+
+    def run(self, code_file):
+        try:
+            check_call(["bash", "-c", 'which -s python3'])
+        except Exception:
+            py_str = "python3"
+        else:
+            py_str = "python"
+        exit(run([py_str, code_file]).returncode)
+
+    def build(self, code, directory=None):
+        with self.file("py", directory=directory) as f:
+            f.write(code)
+        self.run(f.name)
+
+
+class Python2Handler(PythonHandler):
+    """Run Python 2 programs."""
+
+    def run(self, code_file):
+        try:
+            check_call(["bash", "-c", 'which -s python2'])
+        except Exception:
+            py_str = "python2"
+        else:
+            py_str = "python"
+        exit(run([py_str, code_file]).returncode)
